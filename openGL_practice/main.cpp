8d0f236787895fe36dcf0c521bd6fedac6c1e139
Chihyung Wu <chihwu@bu.edu>
2020-04-19 21:15:10 -0400

    Initial Commit

---
+++ openGL_practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openGL_practice
+//
+//  Created by Chihyung Wu on 4/19/20.
+//  Copyright © 2020 Chihyung Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
